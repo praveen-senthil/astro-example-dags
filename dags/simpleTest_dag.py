e83c7f7df172d96b39d40265220fbd5417f6e41a
praveen-senthil <153252328+praveen-senthil@users.noreply.github.com>
2025-02-06 14:41:00 +0530

    Update simpleTest_dag.py

--- dags/simpleTest_dag.py
+++ dags/simpleTest_dag.py
@@ -3,32 +3,32 @@ from airflow.operators.python import PythonOperator
 from airflow.operators.bash import BashOperator
 from datetime import datetime
 
-
 def testing1():
-  print("Testing1...")
+    print("Testing1...")
 
 def testing2():
-  print("Testing2...")
+    print("Testing2...")
 
-default = {
-      "owner" : "airflow",
-      "start_date" : datetime(2025, 6, 2),
-      "retries" : 1
-  }
+default_args = {
+    "owner": "airflow",
+    "start_date": datetime(2025, 2, 6),
+    "retries": 1
+}
 
 with DAG(
-  dag_id = "simpleTest_dag",
-  schedule_interval = '@daily',
-  catchup = False
-) as dag :
-        test1_task = PythonOperator(
-          task_id = 'test1',
-          python_callable  = testing1
-        )
+    dag_id="simpleTest_dag",
+    default_args=default_args,
+    schedule_interval='*/1 * * * *',  # Every minute
+    catchup=False
+) as dag:
+    test1_task = PythonOperator(
+        task_id='test1',
+        python_callable=testing1
+    )
 
-        test2_task = PythonOperator(
-          task_id = 'test2',
-          python_callable = testing2
-        )
+    test2_task = PythonOperator(
+        task_id='test2',
+        python_callable=testing2
+    )
 
-        test2_task >> test1_task
+    test1_task >> test2_task
